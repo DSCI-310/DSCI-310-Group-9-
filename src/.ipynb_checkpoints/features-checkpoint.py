3bf646b6cf916c861b6725908d11d82a7d21b4b1
Allan Cho <allancho994@gmail.com>
2022-03-12 02:16:57 -0800

    finished testing and implementing function

--- src/.ipynb_checkpoints/features-checkpoint.py
+++ src/.ipynb_checkpoints/features-checkpoint.py
@@ -1,9 +1,10 @@
+import pandas as pd
 #' Dataframe of dependent variables
 #'
 #' Creates a new data frame without the given targetValue
 #'
 #' @param data_frame A data frame or data frame extension (e.g. a tibble).
-#' @param target_Value column containing the dependent variable
+#' @param target_Value as String, column containing the dependent variable
 #'
 #' @return A data frame with the given target_Value. 
 #'   Each column (excluding target_Value) lists the possible inputs for the given feature.
@@ -11,5 +12,16 @@
 #' @examples features(car_table, price)
 
 def features(data_frame, target_Value):
-    return data_frame
+    try:
+        if(not isinstance(target_Value, str)):
+            raise RuntimeError("Not a string")
+        if(not isinstance(data_frame, pd.DataFrame)):
+            raise RuntimeError("Not a dataframe")
+        
+        return data_frame.drop(columns=target_Value)
+    except AttributeError as err:
+        print("Not a dataframe, data_frame", err)
+    except RuntimeError:
+        print("Not a string/data frame")
+        
     # return a data frame without one column: targetValue
