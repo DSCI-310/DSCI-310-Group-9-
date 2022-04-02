d4bd16e90dc80eb9ebaa69ae82581d1c5d61eed3
zhangfred8 <zhangfred8@gmail.com>
2022-04-02 01:11:20 -0700

    Fixed visualize_car_data figure

--- src/visualize_car_data.py
+++ src/visualize_car_data.py
@@ -15,10 +15,12 @@ car_table = pd.read_csv(args.file_path, sep=",", header=None,
 
 price = pd.DataFrame(car_table["price"])
 
+
 plt.hist(price, bins=7)
 plt.xlabel("Price")
 plt.ylabel("Count")
 plt.title("Figure 1: Distribution of Price Variable")
-plt.figure(figsize=(10, 10), dpi= 80)
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
 
 plt.savefig(args.out_file_path)
